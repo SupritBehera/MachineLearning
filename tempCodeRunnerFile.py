93ea523129fa25f4124fe7d14bf6bcde93ae8292
Suprit Behera <supritb12@gmail.com>
2021-03-09 14:41:54 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-      if(line[3] == '0'):
-        cluster0.append(line[1])
-      if(line[3] == '1'):
-        cluster1.append(line[1])
-      if(line[3] == '2'):
-        cluster2.append(line[1])
